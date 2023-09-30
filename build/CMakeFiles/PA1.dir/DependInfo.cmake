11dbb11b18710c4537097a814d162bc16f3cef51
Josh Abbott <122472243+Josh-Abbott@users.noreply.github.com>
2023-09-29 17:23:05 -0700

    Initialize to GitHub.

--- build/CMakeFiles/PA1.dir/DependInfo.cmake
+++ build/CMakeFiles/PA1.dir/DependInfo.cmake
@@ -8,8 +8,8 @@ set(CMAKE_DEPENDS_LANGUAGES
 
 # The set of dependency files which are needed:
 set(CMAKE_DEPENDS_DEPENDENCY_FILES
-  "/home/josh_abbott/PA1/main.cpp" "CMakeFiles/PA1.dir/main.cpp.o" "gcc" "CMakeFiles/PA1.dir/main.cpp.o.d"
-  "/home/josh_abbott/PA1/menu.cpp" "CMakeFiles/PA1.dir/menu.cpp.o" "gcc" "CMakeFiles/PA1.dir/menu.cpp.o.d"
+  "C:/Users/lucar/OneDrive/Documents/WSU/Previous Semesters/Fall 2022/CPTS 223/Programming Assignments/PA1Canvas/PA1/main.cpp" "CMakeFiles/PA1.dir/main.cpp.obj" "gcc" "CMakeFiles/PA1.dir/main.cpp.obj.d"
+  "C:/Users/lucar/OneDrive/Documents/WSU/Previous Semesters/Fall 2022/CPTS 223/Programming Assignments/PA1Canvas/PA1/menu.cpp" "CMakeFiles/PA1.dir/menu.cpp.obj" "gcc" "CMakeFiles/PA1.dir/menu.cpp.obj.d"
   )
 
 # Targets to which this target links.
--- build/CMakeFiles/PA1.dir/cmake_clean.cmake
+++ build/CMakeFiles/PA1.dir/cmake_clean.cmake
@@ -1,10 +1,12 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/PA1.dir/main.cpp.o"
-  "CMakeFiles/PA1.dir/main.cpp.o.d"
-  "CMakeFiles/PA1.dir/menu.cpp.o"
-  "CMakeFiles/PA1.dir/menu.cpp.o.d"
-  "PA1"
+  "CMakeFiles/PA1.dir/main.cpp.obj"
+  "CMakeFiles/PA1.dir/main.cpp.obj.d"
+  "CMakeFiles/PA1.dir/menu.cpp.obj"
+  "CMakeFiles/PA1.dir/menu.cpp.obj.d"
+  "PA1.exe"
+  "PA1.exe.manifest"
   "PA1.pdb"
+  "libPA1.dll.a"
 )
 
 # Per-language clean rules from dependency scanning.
